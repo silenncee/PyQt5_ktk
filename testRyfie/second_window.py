fb09f653e1bd87cbb1b26ed65b4ca284e7c08067
nastea <anastasiaanasta61@gmail.com>
2025-05-11 22:42:53 +0500

    Update second_window.py

--- testRyfie/second_window.py
+++ testRyfie/second_window.py
@@ -1,97 +1,177 @@
-from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout, QLineEdit, QPushButton
-import instr
+from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout, QLineEdit, QPushButton, QMessageBox, QHBoxLayout
 from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtGui import QFont
-from third_window import ThirdWin
+from third_window import FinalWin
+import instr
 
-time = QTime(0, 0, 15)
-# time.addSecs(-1)
-# time.toString("hh:mm:ss")
 
 class TestWin(QWidget):
-    def timer1event(self):
-
-        global time
-    # time = QTime(0, 1, 0)
-        time = time.AddSecs(-1)
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
         self.timer = QTimer()
-        self.timer.timeout.connect(self.self.timer1event)
-        self.timer.start(1000)
-        self.timer.stop()
-        self.text_timer.setText(time.toString("hh:mm:ss"))
-        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.time = QTime(0, 0, 0)  # Начальное время
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+        self.timer_layout = QHBoxLayout()
+
+        self.text_timer = QLabel("00:00:15")
+        self.text_timer.setFont(QFont("Times", 20, QFont.Bold))
         self.text_timer.setStyleSheet("color: rgb(0, 0, 0)")
-        if time.toString("hh:mm:ss") == "00:00:00":
+
+        self.timer_layout.addStretch()
+        self.timer_layout.addWidget(self.text_timer, alignment=Qt.AlignRight)
+
+        self.layout.addLayout(self.timer_layout)
+
+
+
+
+        self.setLayout(self.layout)
+
+    def start_timer1(self):
+        self.time = QTime(0, 0, 15)
+        self.timer.timeout.connect(self.timer1_event)
+        self.timer.start(1000)
+        self.timer1_event()
+
+    def timer1_event(self):
+        self.time = self.time.addSecs(-1)
+        self.text_timer.setText(self.time.toString("mm:ss"))
+
+        if self.time == QTime(0, 0):
+
+            self.timer.stop()
+            
+
+
+    def start_timer2(self):
+        self.time = QTime(0, 0, 30)
+        self.timer.timeout.connect(self.timer2_event)
+        self.timer.start(1000)
+        self.timer2_event()
+
+    def timer2_event(self):
+        self.time = self.time.addSecs(-1)
+        self.text_timer.setText(self.time.toString("mm:ss"))
+        if self.time == QTime(0, 0):
             self.timer.stop()
 
 
-    def connects(self):
-        self.bth_test1.clicked.connect(self.timer_test)
-        self.bth_test1.clicked.connect(self.timer_test)
+    def start_timer3(self):
+        self.time = QTime(0, 0, 59)
+        self.timer.timeout.connect(self.timer3_event)
+        self.timer.start(1000)
+        self.timer3_event()
 
+    def timer3_event(self):
+        self.time = self.time.addSecs(-1)
+        if self.time >= QTime(0, 0, 45):
+            self.text_timer.setStyleSheet("color: green")
+        elif self.time >= QTime(0, 0, 15):
+            self.text_timer.setStyleSheet("color: black")
+        else:
+            self.text_timer.setStyleSheet("color: red")
+
+        self.text_timer.setText(self.time.toString("mm:ss"))
+        if self.time == QTime(0, 0):
+            self.timer.stop()
 
 
 class SecondWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.initui()
+        self.init_ui()
 
-    def initui(self):
+    def init_ui(self):
         self.setWindowTitle("Результат")
-        self.resize(400, 300)
+        self.resize(800, 400)
         self.move(1000, 600)
 
-        self.layout = QVBoxLayout()
+        self.layout = QHBoxLayout()
+        self.input_layout = QVBoxLayout()
 
         self.fio_label = QLabel("Введите ФИО:")
-        self.layout.addWidget(self.fio_label)
+        self.input_layout.addWidget(self.fio_label)
 
         self.fio_input = QLineEdit()
         self.fio_input.setFixedWidth(200)
-        self.layout.addWidget(self.fio_input)
+        self.input_layout.addWidget(self.fio_input)
 
         self.age_label = QLabel("Введите полных лет:")
-        self.layout.addWidget(self.age_label)
+        self.input_layout.addWidget(self.age_label)
 
-        self.age_input = QLineEdit()
+        self.age_input = QLineEdit()  #возраст инпут
         self.age_input.setFixedWidth(200)
-        self.layout.addWidget(self.age_input)
+        self.input_layout.addWidget(self.age_input)
 
         self.info_text = QLabel(instr.text2)
-        self.layout.addWidget(self.info_text)
-
-        self.bth_test1 = QPushButton("Начать первый тест")
-        self.bth_test1.setFixedWidth(150)
-        self.layout.addWidget(self.bth_test1)
+        self.input_layout.addWidget(self.info_text)
 
         self.additional_input = QLineEdit()
         self.additional_input.setFixedWidth(200)
-        self.layout.addWidget(self.additional_input)
+        self.input_layout.addWidget(self.additional_input)             #инпут. пульс1
 
         self.additional_text = QLabel(instr.text3)
-        self.layout.addWidget(self.additional_text)
+        self.input_layout.addWidget(self.additional_text)
+
+        self.test1_button = QPushButton("Начать первый тест")
+        self.test1_button.setFixedWidth(150)
+        self.test1_button.clicked.connect(self.start_test1)
+        self.input_layout.addWidget(self.test1_button)
 
         self.start_squats_button = QPushButton("Начать делать приседания")
         self.start_squats_button.setFixedWidth(150)
-        self.layout.addWidget(self.start_squats_button)
+        self.start_squats_button.clicked.connect(self.start_squats)
+        self.input_layout.addWidget(self.start_squats_button)
 
         self.squats_info_text = QLabel(instr.text4)
-        self.layout.addWidget(self.squats_info_text)
+        self.input_layout.addWidget(self.squats_info_text)
 
         self.final_button = QPushButton("Начать финальный тест")
         self.final_button.setFixedWidth(150)
-        self.layout.addWidget(self.final_button)
+        self.final_button.clicked.connect(self.start_final_test)
+        self.input_layout.addWidget(self.final_button)
 
+        self.fin_input = QLineEdit()
+        self.fin_input.setFixedWidth(200)                #инпуты
+        self.input_layout.addWidget(self.fin_input)
+
+        self.fin2_input = QLineEdit()
+        self.fin2_input.setFixedWidth(200)
+        self.input_layout.addWidget(self.fin2_input)
 
         self.next_window_button = QPushButton("Отправить результаты")
         self.next_window_button.setFixedWidth(150)
         self.next_window_button.clicked.connect(self.open_third_window)
-        self.layout.addWidget(self.next_window_button)
+        self.input_layout.addWidget(self.next_window_button)
+
+        # Правая часть для TestWin
+        self.test_widget = TestWin()
+        self.layout.addLayout(self.input_layout)  # Добавляем ввод данных
+        self.layout.addWidget(self.test_widget)  # Добавляем TestWin
 
         self.setLayout(self.layout)
 
-    def open_third_window(self):
-        self.third_window = ThirdWin()
-        self.third_window.show()
-        self.close()
+    def start_test1(self):
+        self.test_widget.start_timer1()
 
+    def start_squats(self):
+        self.test_widget.start_timer2()  # Запускаем таймер приседаний
+
+    def start_final_test(self):
+        self.test_widget.start_timer3()
+
+
+    def open_third_window(self):
+        age_value = self.age_input.text()
+        additional_value = self.additional_input.text()
+        fin_value = self.fin_input.text()
+        fin2_value = self.fin2_input.text()
+
+        # Создаем экземпляр FinalWin и передаем значения
+        self.final_window = FinalWin(age_value, additional_value, fin_value, fin2_value)
+        self.final_window.show()  # Показываем окно с индексом Руфье
+
+        self.close()  # Закрываем текущее окно
